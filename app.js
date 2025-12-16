e561175eb3d5cff873c0a011cc0b5ebb0423527c
ansharahamin <insharahamin1250@gmail.com>
2025-12-16 18:04:26 +0500

    practice

---
+++ app.js
@@ -0,0 +1,71 @@
+// let obj = { name: "ansharah", age: 20, city: "karachi" };
+// let { name, age, city } = obj;
+// console.log(name);
+
+// let obj = { name: "ansharah", age: 20, city: "karachi" , country: "Pakistan"};
+// let { name, age, ...address } = obj;
+// console.log(address);
+
+// function sum(...numbers){ // rest operator
+//     let result = 0;
+//     for (let nums of numbers){
+//         result += nums;
+//     }
+//     return result;
+// }
+
+// console.log(sum(1,2,3,4,5));
+// let arr = [1,2,3,4,5]; // spread operator
+// console.log(sum(...arr));
+
+
+// let arr = [1,2,[3,4],[5,6]];
+// let [a,b , ...rest] = arr;
+// console.log(rest);
+
+
+// let arr = [1,2,[3,4],[5,6]];
+// let [a,b ,[c,d]] = arr;
+// console.log(d);
+
+// let obj = {
+//     name: "ansharah",
+//     age: 20,
+//     address: {
+//         city: "karachi",
+//         country: "Pakistan"
+//     }
+// };
+// let {name , age , ...address } = obj;
+// console.log(address);          
+
+
+//obj in object destructuring with rest operator
+// let obj = {
+//     name: "ansharah",
+//     age: 20,
+//     address: {
+//         city: "karachi",
+//         country: "Pakistan"
+//     }
+// };
+// let {name , age ,address:{city,country}} = obj;
+// console.log(city);          
+// console.log(country);          
+
+// Enhance object literals
+
+
+// let name = "ansharah";  
+// let age = 20;
+// let city = "karachi";
+// let person = {name,age,city};
+// console.log(person);
+
+
+// let name = "ansharah";  
+// let age = 20;
+// let city = "karachi";
+// let person = {name,age,city};
+// console.log(person);
+
