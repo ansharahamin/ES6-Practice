b0048044665a4af82be04c99415013cd381fea18
ansharahamin <insharahamin1250@gmail.com>
2025-12-21 17:13:21 +0500

    inheritance

--- bank account/class inheritance/app.js
+++ bank account/class inheritance/app.js
@@ -18,11 +18,18 @@ class Employee {
   }
 }
 class programmer extends Employee {
+  constructor(name,designation,salary,team){
+    super(name,designation,salary)
+    this.team = team
+  }
   lang(lang) {
     console.log(`i use ${lang} language`);
   }
-super.hours()
-console.log();
+distribution(coding,debugging){
+super.hours(coding + debugging)
+console.log(`${coding} coding ${debugging} debugging`);
+
+}
 
 }
 class developer extends programmer {
@@ -78,5 +85,12 @@ class fullStackDeveloper extends WebDeveloper {
 
 let ansharah = new fullStackDeveloper("Ansharah", "programmer", "500000");
 console.log(ansharah.deployProject());
-let sameen = new backendDeveloper("Ansharah", "programmer", "500000");
-console.log(ansharah.deployProject());
+let sameen = new backendDeveloper("Sameen", "programmer", "500000");
+console.log(sameen.manageDatabase());
+let umra = new frontEnd("Umra","developer" , 99099)
+console.log(umra.createUI());
+let Ahmed = new programmer("Ahmed","dev" , "89898")
+console.log(Ahmed.lang("English"));
+let Neha = new programmer("Neha","dev" , "89898")
+console.log(Ahmed.distribution(5,10));
+
