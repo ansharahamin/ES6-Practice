e9eacebdaf564fded7850018b7fed24db1e86f02
ansharahamin <insharahamin1250@gmail.com>
2026-01-02 18:04:15 +0500

    presentation module

--- module/app.js
+++ module/app.js
@@ -1,2 +1,19 @@
-import Fname from "authentication.js"
-console.log(Fname.sum);
+import Fname,{multiply} from "./authentication.js"
+
+console.log( Fname);
+
+multiply(6,7)
+
+// console.log(Fname.sum);
+// console.log(Fname.name);
+
+
+// destructuring of Obj
+
+
+// let {name , sub} = Fname
+// // console.log(sum);
+// console.log(name);
+// sum(5,3)
+
+
--- module/authentication.js
+++ module/authentication.js
@@ -1,6 +1,24 @@
-let name = "inshirah"
+// let name = "inshirah"
 
-let sum = (a,b)=>{
-console.log(a+b);
+// let sum = (a,b)=>{
+// console.log(a+b);
+// }
+
+
+export default function sub(c,d){
+
+    console.log(c-d);
+    
+
 }
-export default {name,sum}
+
+export function multiply(x,z) {
+    
+    console.log(x*z);
+}
+
+// export default {name,sum}
+
+// Named Export 
+
+
